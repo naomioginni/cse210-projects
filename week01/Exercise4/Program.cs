3aed87c4c6af16a0ef9008b77d3bf51f124f5942
naomioginni <noginni@byupathway.edu>
2026-07-03 14:00:13 +0100

    Exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int number = -1;
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+
+        int sum = 0;
+        foreach (int n in numbers)
+        {
+            sum += n;
+        }
+
+        float average = (float)sum / numbers.Count;
+
+        int max = numbers[0];
+        foreach (int n in numbers)
+        {
+            if (n > max)
+            {
+                max = n;
+            }
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
